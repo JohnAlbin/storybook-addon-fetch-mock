a58c68f9ccf4e96247c446ca3547180cdd2cb6f3
JohnAlbin <virtually.johnalbin@gmail.com>
2022-07-07 01:05:42 +0800

    chore: Add build scripts

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  env: {
+    esm: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+          },
+        ],
+      ],
+    },
+  },
+};
